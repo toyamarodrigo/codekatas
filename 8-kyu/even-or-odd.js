6451e351b335f7bda707d614d7c010a428e247f4
Rodrigo Toyama <toyama.rodrigo@gmail.com>
2020-09-29 19:29:05 -0300

    added 8kyu and 7kyu ex

---
+++ 8-kyu/even-or-odd.js
@@ -0,0 +1,8 @@
+// Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const even_or_odd = (number) => (number % 2 === 0 ? 'Even' : 'Odd');
+
+console.log((even_or_odd(2), 'Even'));
+console.log((even_or_odd(0), 'Even'));
+console.log((even_or_odd(7), 'Odd'));
+console.log((even_or_odd(1), 'Odd'));
